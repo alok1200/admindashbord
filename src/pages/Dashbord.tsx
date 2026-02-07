6bc3ec80ad13655d57aae27a180ae102bbcf67a4
Alok Singh <salok2857@gmail.com>
2026-02-07 18:28:34 +0530

    same as dashbord

--- src/pages/Dashbord.tsx
+++ src/pages/Dashbord.tsx
@@ -1,3 +1,4 @@
+import { HiTrendingDown, HiTrendingUp } from 'react-icons/hi';
 import AdminSideBar from '../component/adminSideBar';
 import { BsSearch } from 'react-icons/bs';
 
@@ -13,8 +14,51 @@ const Dashbord = () => {
           <input type="text" placeholder="Search for data users docs" />
           <img src={userImg} alt="User" />
         </div>
+        <section className="Widgatcontainer">
+          <WidgetItom
+            heading="Revenue"
+            value={1234}
+            percent={12}
+            color="green"
+            amount
+          />
+        </section>
       </main>
     </div>
+  );
+};
+
+interface WidgetItomprop {
+  heading: string;
+  percent: number;
+  value: number;
+  color: string;
+  amount?: boolean;
+}
+
+const WidgetItom = ({
+  heading,
+  value,
+  percent,
+  color,
+  amount,
+}: WidgetItomprop) => {
+  return (
+    <article className="widget">
+      <div className="widgetinfo">
+        <p>{heading}</p>
+        <h4>{amount ? `$${value}` : value}</h4>
+        {percent > 0 ? (
+          <span className={color}>
+            <HiTrendingUp /> + {percent}% {''}
+          </span>
+        ) : (
+          <span className={color}>
+            <HiTrendingDown /> - {percent}% {''}
+          </span>
+        )}
+      </div>
+    </article>
   );
 };
 
